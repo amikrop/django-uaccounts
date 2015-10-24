5bf113629f982bc323fa3f5e488eb50dfec4bac9
Aristotelis Mikropoulos <amikrop@gmail.com>
2015-10-24 22:23:50 +0300

    Docstring fixes

--- uaccounts/views.py
+++ uaccounts/views.py
@@ -19,11 +19,11 @@ def index(request, template_name='uaccounts/index.html'):
     """User's homepage.
 
     **context**
-    `primary`: primary email
-    `secondary`: list of verified emails
-    `unverified`: list of unverified emails
-    `count`: total email count
-    `home`: parent url
+    - `primary`: primary email
+    - `secondary`: list of verified emails
+    - `unverified`: list of unverified emails
+    - `count`: total email count
+    - `home`: parent url
     """
     context = profile_emails(request.user.profile)
     if HOME_URL != reverse('uaccounts:index'):
@@ -39,8 +39,8 @@ def log_in(request, template_name='uaccounts/login.html',
     If they are already logged in, redirect to index.
 
     **context**
-    `form`: login form
-    `error`: error message
+    - `form`: login form
+    - `error`: error message
     """
     form = forms.LoginForm()
 
@@ -86,7 +86,7 @@ def register(request, template_name='uaccounts/register.html'):
     If they are logged in, redirect to index.
 
     **context**
-    `form`: registration form
+    - `form`: registration form
     """
     form = forms.RegistrationForm()
 
@@ -146,9 +146,9 @@ def forgot(request, template_name='uaccounts/forgot.html',
     send the respective email, or just show the "forgot password" page.
 
     **context**
-    `email`: email address the mail was sent to
-    `form`: "forgot password" form
-    `error`: error message
+    - `email`: email address the mail was sent to
+    - `form`: "forgot password" form
+    - `error`: error message
     """
     form = forms.EmailAddressForm()
 
@@ -180,7 +180,7 @@ def change(request, token,
     or try to change the password.
 
     **context**
-    `form`: "change password" form
+    - `form`: "change password" form
     """
     try:
         verification = verify_token(token, CHANGE_PASSWORD_EXPIRES)
@@ -208,7 +208,7 @@ def edit(request, template_name='uaccounts/edit.html'):
     """Show "edit profile" page or process the profile editing.
 
     **context**
-    `form`: "edit profile" form
+    - `form`: "edit profile" form
     """
     profile_form = forms.EditProfileForm(instance=request.user.profile)
     user_form = forms.EditUserForm(instance=request.user)
